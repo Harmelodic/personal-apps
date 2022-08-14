ba34e70662d8738b52725484cd3fdeebf6e64964
Harmelodic <matt@harmelodic.com>
2022-08-14 03:00:53 +0200

    Adding location

--- infrastructure/lookups.tf
+++ infrastructure/lookups.tf
@@ -16,9 +16,16 @@ data "google_project" "apps" {
   project_id = data.google_projects.apps_lookup.projects[0].project_id
 }
 
+variable "apps_gke_location" {
+  description = "Location of where GKE cluster will be created"
+  sensitive   = true
+  type        = string
+}
+
 data "google_container_cluster" "apps" {
-  name    = "apps"
-  project = data.google_project.apps.project_id
+  location = var.apps_gke_location
+  name     = "apps"
+  project  = data.google_project.apps.project_id
 }
 
 data "google_sql_database_instance" "apps" {
