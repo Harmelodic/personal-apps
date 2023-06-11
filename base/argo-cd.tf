68969359f9a47425a58ced75f7f2770ba67c0c45
Harmelodic <matt@harmelodic.com>
2023-06-11 12:20:53 +0200

    Pod Security labels for argo and cert-manager

--- base/argo-cd.tf
+++ base/argo-cd.tf
@@ -3,7 +3,12 @@ resource "kubernetes_namespace" "argo_cd" {
     name = "argocd"
 
     labels = {
-      environment = terraform.workspace
+      "pod-security.kubernetes.io/enforce"         = "privileged"
+      "pod-security.kubernetes.io/enforce-version" = "latest"
+      "pod-security.kubernetes.io/audit"           = "baseline"
+      "pod-security.kubernetes.io/audit-version"   = "latest"
+      "pod-security.kubernetes.io/warn"            = "restricted"
+      "pod-security.kubernetes.io/warn-version"    = "latest"
     }
   }
 }
--- base/cert-manager-workload.tf
+++ base/cert-manager-workload.tf
@@ -1,6 +1,15 @@
 resource "kubernetes_namespace" "cert_manager" {
   metadata {
     name = "cert-manager"
+
+    labels = {
+      "pod-security.kubernetes.io/enforce"         = "privileged"
+      "pod-security.kubernetes.io/enforce-version" = "latest"
+      "pod-security.kubernetes.io/audit"           = "baseline"
+      "pod-security.kubernetes.io/audit-version"   = "latest"
+      "pod-security.kubernetes.io/warn"            = "restricted"
+      "pod-security.kubernetes.io/warn-version"    = "latest"
+    }
   }
 }
 
